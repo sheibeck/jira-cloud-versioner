dc5b1e1dcdf1eedcb1238599c959f1be52f1994d
Sterling Heibeck <sheibeck@dealeron.com>
2022-12-12 18:37:57 -0500

    show and hide

--- src/content-scripts/App.vue
+++ src/content-scripts/App.vue
@@ -2,13 +2,13 @@
   <!-- Latest compiled and minified CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-  <div id="jcv-root" class="container">
+  <div id="jcv-root" class="container" :class="{ hidden: !isVisible }">
     <div class="row">      
       <div class="col-11">
         <div class="h2">Jira Version Manager</div>
       </div>
       <div class="col-1">
-        <button type="button" class="btn btn-secondary">Close</button>
+        <button type="button" class="btn btn-secondary" @click="toggleApp()">Close</button>
       </div>
     </div>
     
@@ -42,8 +42,25 @@ class JiraTicket {
 }
 
 const tickets = ref(new Array<string>());
+let isVisible = ref(false);
 
-onMounted(() => {   
+const toggleApp = function() {
+  isVisible.value = !isVisible.value;
+  
+  const appRoot = document.getElementById("jcvApp");
+  if (isVisible.value) {
+    appRoot?.classList.remove("hidden");
+  }
+  else {
+    appRoot?.classList.add("hidden");
+  }
+}
+
+onMounted(() => {  
+  document.addEventListener("openJcv", (event) => {    
+    toggleApp();
+  });
+
   const integrationColumnId = document.querySelector("[title='Integrating']")?.getAttribute("data-id");
   const swimLanes = document.querySelectorAll(`[data-column-id="${integrationColumnId}"]`);
 
@@ -69,21 +86,20 @@ onMounted(() => {
 </script>
 
 <style>
-#app { 
+#jcvApp { 
   position: absolute;
   left: 0;
   top: 0;  
-  z-index: 999;
   height: 100%;
   width: 100%;
+  z-index: 999;
 }
 
 #jcv-root { 
-  height: 100%;
-  width: 100%;
+  overflow: scroll;
   border: solid 1px black;
   background-color: black;
-  
-  overflow: scroll;
+  height: 100%;
+  width: 100%;
 }
 </style>
--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -2,14 +2,33 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 
+const openJcvEvent = new CustomEvent('openJcv');
+
 window.onload = async () => {  
   const el = document.querySelector('body');
   if (el) {
     el.insertAdjacentHTML(
       'afterend',
-      '<div id="app">Hello World</div>',
+      '<div id="jcvApp" class="hidden">Hello World</div>',
     );
     const app = createApp(App).use(createPinia());
-    app.mount('#app');
+    app.mount('#jcvApp');
+  }
+  
+  const createButton = document.querySelector("nav");
+  if (createButton) {
+    createButton.insertAdjacentHTML(
+      'afterend',
+      `<div id="jcv-button">
+        <button type="button">V!</button>
+      </div>`,
+    );
+
+    const jcvButton = document.getElementById("jcv-button");
+    if (jcvButton) {
+      jcvButton.addEventListener('click', () => {
+        document.dispatchEvent(openJcvEvent);
+      });
+    }
   }
 }
